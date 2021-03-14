196b0a2213b35fb8098bf9990dabe017f57eff20
yamyem <64607281+yamyem@users.noreply.github.com>
2021-03-14 15:20:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const svg = document.querySelector('.background-svg');
+const strokes = document.querySelectorAll('.background-stroke');
+const card1 = document.querySelector('.card:nth-of-type(1)');
+const card2 = document.querySelector('.card:nth-of-type(2)');
+const card3 = document.querySelector('.card:nth-of-type(3)');
+
+
+card1.addEventListener('mouseover', function(){
+    svg.style.cssText = "fill: hsl(240, 100%, 60%); transition: all 0.5s ease;";
+    strokes.forEach(function(j){
+        j.style.cssText = "stroke: hsl(240, 100%, 60%); transition: all 0.5s ease;";
+    })
+})
+card3.addEventListener('mouseover', function(){
+    svg.style.cssText = "fill: #ffff00; transition: all 0.5s ease;";
+    strokes.forEach(function(j){
+        j.style.cssText = "stroke: #ffff00; transition: all 0.5s ease;";
+    })
+})
+card2.addEventListener('mouseover', function(){
+    svg.style.cssText = "fill: #ff0000; transition: all 0.5s ease;";
+    strokes.forEach(function(j){
+        j.style.cssText = "stroke: #ff0000; transition: all 0.5s ease;";
+    })
+})
