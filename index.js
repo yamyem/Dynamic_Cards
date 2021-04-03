b59db5a1142665c97cbf0beb7bed4c428d5fb49f
Joanna <burlikowskajoanna@gmail.com>
2021-04-03 11:04:11 +0200

    changes in animation and in layout

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const card3 = document.querySelector('.card:nth-of-type(3)');
 
 
 card1.addEventListener('mouseover', function(){
-    svg.style.cssText = "fill: hsl(240, 100%, 60%); transition: all 0.5s ease;";
+    svg.style.cssText = "fill: hsl(240, 100%, 60%); transform:translateX(-400px); transition: all 0.5s ease;";
     strokes.forEach(function(j){
         j.style.cssText = "stroke: hsl(240, 100%, 60%); transition: all 0.5s ease;";
     })
 })
 card3.addEventListener('mouseover', function(){
-    svg.style.cssText = "fill: #ffff00; transition: all 0.5s ease;";
+    svg.style.cssText = "fill: #ffff00;  transition: all 0.5s ease;";
     strokes.forEach(function(j){
         j.style.cssText = "stroke: #ffff00; transition: all 0.5s ease;";
     })
 })
 card2.addEventListener('mouseover', function(){
-    svg.style.cssText = "fill: #ff0000; transition: all 0.5s ease;";
+    svg.style.cssText = "fill: #ff0000; transform:translateX(-200px); transition: all 0.5s ease;";
     strokes.forEach(function(j){
         j.style.cssText = "stroke: #ff0000; transition: all 0.5s ease;";
     })
 })
+
+function animate() {
+    if(Event.type =='mouseover'){
+
+    }
+}
